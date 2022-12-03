94fe73501f4c838b16133403537d417ae3510264
baludbr2100030140 <2100030140@kluniversity.in>
2022-12-03 23:10:47 +0530

    add

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const cors = require('cors');
 
 dotenv.config()
 
-mongoose.connect(process.env.DATABASE_ACCESS,()=>{
+mongoose.connect(`mongodb+srv://baludbr:baludbr@cluster0.ghivz7m.mongodb.net/sdp1?retryWrites=true&w=majority`,()=>{
     console.log("Database Connected!!")
 })
 
 app.use(express.json())
 app.use(cors());
 app.use('/api',routesurls);
-app.listen(2000,()=>{console.log("Server Started on 2000")})
+app.listen(process.env.PORT||2000,()=>{console.log("Server Started on 2000")})
